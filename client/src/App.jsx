d1d75555f1ba3536ec6b2146577bcd844f165232
Arjuna Anuhass <anuhass2017@gmail.com>
2025-05-08 19:08:17 +0530

    created a CeckAuth function and implement some conditions  to relavant authorization (role)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,28 +13,49 @@ import ShoppingHome from "./pages/shopping-view/shoppingView.page.home";
 import ShoppingListing from "./pages/shopping-view/shoppingView.page.listing";
 import ShoppingCheckout from "./pages/shopping-view/shoppingView.page.checkout";
 import ShoppingAccount from "./pages/shopping-view/shoppingView.page.account";
+import CheckAuth from "./components/common/common.comp.check-auth";
+import UnauthPage from "./pages/unauth-page/unauthPage.page.index";
 
 
 
 function App() {
-  return ( 
+
+  const isAuthenticated = true;
+  const user = {
+    name: "Anuhass",
+    role: "admin"
+  };
+
+  return (
     <div className="flex flex-col overflow-hidden bg-white">
 
       <Routes>
         {/* ------auth routers------ */}
-        <Route path="/auth" element={<AuthLayout/>}>
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AuthLayout/>
+          </CheckAuth>
+        }>
           <Route path="login" element={<AuthLogin/>}/>
           <Route path="register" element={<AuthRegister/>}/>
         </Route>
         {/* ------admin routers--------- */}
-        <Route path="/admin" element={<AdminLayout/>}>
+        <Route path="/admin" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AdminLayout/>
+          </CheckAuth>
+        }>
           <Route path="dashboard" element={<AdminDashboard/>}/>
           <Route path="products" element={<AdminProducts/>}/>
           <Route path="orders" element={<AdminOrders/>}/>
           <Route path="features" element={<AdminFeatures/>}/>
         </Route>
         {/* -------shopping routers------- */}
-        <Route path="/shop" element={<ShoppingLayout/>}>
+        <Route path="/shop" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <ShoppingLayout/>
+          </CheckAuth>
+        }>
           <Route path="home" element={<ShoppingHome/>}/>
           <Route path="listing" element={<ShoppingListing/>}/>
           <Route path="checkout" element={<ShoppingCheckout/>}/>
@@ -42,6 +63,7 @@ function App() {
         </Route>
 
         <Route path="*" element={<NotFound/>}/>
+        <Route path="/unauth-page" element={<UnauthPage/>}/>
       </Routes>
     </div>
    );

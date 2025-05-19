f6c1c7d5d374feb24567c001fec2db499cdf19e1
Arjuna Anuhass <anuhass2017@gmail.com>
2025-05-19 15:00:01 +0530

    Admin header and sidebar created successfully

--- client/src/components/admin-view/adminView.comp.header.jsx
+++ client/src/components/admin-view/adminView.comp.header.jsx
@@ -1,9 +1,23 @@
+import { Button } from "../ui/button";
+import { Menu, LogOut } from "lucide-react";
 
 
 
-function AdminHeader() {
+function AdminHeader({setOpen}) {
     return ( 
-        <div>admin header</div>
+        <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+            <Button onClick = {() => setOpen(true)} className="lg:hidden sm:block">
+                <Menu />
+                <span className="sr-only">Toggle Menu</span>
+            </Button>
+
+            <div className="flex flex-1 justify-end">
+                <Button className="inline-flex gap-2 items-center rounded-md px-4 text-sm font-medium shadow">
+                    <LogOut />
+                    Logout
+                </Button>
+            </div>
+        </header>
      );
 }
 

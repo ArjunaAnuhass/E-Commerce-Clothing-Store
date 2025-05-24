d3c57b07a6d171ac7c2a7f62711811c7661425b3
Arjuna Anuhass <anuhass2017@gmail.com>
2025-05-24 15:01:33 +0530

    created Product model and add, fetch all, edit and delete method in products and tested successfully

--- server/controller/admin/admin.product-controller.js
+++ server/controller/admin/admin.product-controller.js
@@ -1,4 +1,5 @@
 import { imageUploadUtil } from "../../config/cloudinary.js";
+import Product from "../../models/Product.js";
 
 
 const handleImageUpload = async (req, res) => {
@@ -14,4 +15,85 @@ const handleImageUpload = async (req, res) => {
     }
 }
 
-export { handleImageUpload }
+//add a new product
+const addProduct = async(req, res) => {
+
+    try {
+      const {image, title, description, category, brand, price, salePrice, totalStock} = req.body;
+
+      const newlyCreatedProducts = new Product({
+        image, title, description, category, brand, price, salePrice, totalStock
+      });
+      await newlyCreatedProducts.save();
+      
+      res.status(201).json({success: true, data: newlyCreatedProducts});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: "Error occurred"});
+    }
+}
+
+//fetch all products
+const fetchAllProducts = async(req, res) => {
+
+    try {
+        const listOfProducts = await Product.find({});
+        res.status(200).json({success: true, data: listOfProducts})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: "Error occurred in fetching product data"});
+    }
+}
+
+//edit a product
+const editProduct = async(req, res) => {
+
+    try {
+        const {id} = req.params;
+        const {image, title, description, category, brand, price, salePrice, totalStock} = req.body;
+
+        const findProduct = await Product.findById(id);
+        if (!findProduct) {
+            return res.status(404).json({success: false, message: "Product not found by this id: ", id});
+        }
+
+        findProduct.title = title || findProduct.title;
+        findProduct.description = description || findProduct.description;
+        findProduct.category = category || findProduct.category;
+        findProduct.brand = brand || findProduct.brand;
+        findProduct.price = price || findProduct.price;
+        findProduct.salePrice = salePrice || findProduct.salePrice;
+        findProduct.totalStock = totalStock || findProduct.totalStock;
+        findProduct.image = image || findProduct.image;
+
+        await findProduct.save();
+        
+        res.status(200).json({success: true, message: "Product edited successfully",  data: findProduct});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: "Error occurred in edit product data"});
+    }
+}
+
+//delete a product
+const deleteProduct = async(req, res) => {
+
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({success: false, message: "Product not found by this id: ", id});
+        }
+        else{
+            return res.status(200).json({success: true, message: "Product Deleted successfully"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: "Error occurred in delete product"});
+    }
+}
+
+
+
+export { handleImageUpload, addProduct, fetchAllProducts, editProduct, deleteProduct }
--- server/routes/admin/admin.products-routes.js
+++ server/routes/admin/admin.products-routes.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { upload } from '../../config/cloudinary.js';
-import { handleImageUpload } from '../../controller/admin/admin.product-controller.js';
+import { addProduct, deleteProduct, editProduct, fetchAllProducts, handleImageUpload } from '../../controller/admin/admin.product-controller.js';
 
 const adminProductRoutes = express.Router();
 
 adminProductRoutes.post('/upload-image', upload.single("my_file"), handleImageUpload)
+adminProductRoutes.post('/add', addProduct);
+adminProductRoutes.put('/edit/:id', editProduct);
+adminProductRoutes.delete('/delete/:id', deleteProduct);
+adminProductRoutes.get('/get', fetchAllProducts);
 
 export default adminProductRoutes;

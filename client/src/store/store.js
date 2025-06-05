fe65a18da958b12b7621b8eac39f1652c888d0b6
Arjuna Anuhass <anuhass2017@gmail.com>
2025-06-05 16:10:44 +0530

    shopping cart slices and store updated successfully

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './auth-slice/auth-slice.store.index'
 import AdminProductSlice from './admin/product-slice/product-slice.admin.store.index'
 import ShoppingProductSlice from './shop/product-slice/product-slice.shop.index'
+import ShoppingCartSlice from './shop/cart-slice/cart-slice.shop.index'
 
 
 const store = configureStore({
@@ -9,6 +10,7 @@ const store = configureStore({
         auth : authReducer,
         adminProducts : AdminProductSlice,
         shopProducts: ShoppingProductSlice,
+        shopCart: ShoppingCartSlice
     },
 });
 

5b1ad3199af6810c36ff4a024d19855708271d9a
Arjuna Anuhass <anuhass2017@gmail.com>
2025-06-19 15:19:59 +0530

    addres redux reducers state management created successfuly with redux

---
+++ client/src/store/shop/address-slice/address-slice.shop.index.js
@@ -0,0 +1,82 @@
+import axios from "axios"
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+
+
+
+const initialState = {
+    isLoading: false,
+    addressList: []
+}
+
+export const addNewAddress = createAsyncThunk(
+    '/address/addNewAddress',
+
+    async (formData) => {
+        const response = await axios.post('http://localhost:3000/api/shop/address/add', formData);
+
+        return response.data;
+    }
+)
+
+export const fetchAllAddresses = createAsyncThunk(
+    '/address/fetchAllAddresses',
+
+    async (userId) => {
+        const response = await axios.get(`http://localhost:3000/api/shop/address/get/${userId}`);
+
+        return response.data
+    }
+)
+
+export const updateAddress = createAsyncThunk(
+    '/address/updateAddress',
+
+    async ({userId, addressId, formData}) => {
+        const response = await axios.put(`http://localhost:3000/api/shop/address/update/${userId}/${addressId}`, formData);
+
+        return response.data;
+    }
+)
+
+export const deleteAddress = createAsyncThunk(
+    '/address/deleteAddress',
+
+    async ({userId, addressId}) => {
+        const response = await axios.delete(`http://localhost:3000/api/shop/address/delete/${userId}/${addressId}`);
+
+        return response.data;
+    }
+)
+
+const AddressSlice = createSlice({
+    name: 'address',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(addNewAddress.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(addNewAddress.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.addressList = action.payload
+            })
+            .addCase(addNewAddress.rejected, (state) => {
+                state.isLoading = false
+                state.addressList = []
+            })
+            .addCase(fetchAllAddresses.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(fetchAllAddresses.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.addressList = action.payload
+            })
+            .addCase(fetchAllAddresses.rejected, (state) => {
+                state.isLoading = false
+            })
+    }
+});
+
+export default AddressSlice.reducer
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,6 +3,7 @@ import authReducer from './auth-slice/auth-slice.store.index'
 import AdminProductSlice from './admin/product-slice/product-slice.admin.store.index'
 import ShoppingProductSlice from './shop/product-slice/product-slice.shop.index'
 import ShoppingCartSlice from './shop/cart-slice/cart-slice.shop.index'
+import AddressSlice from './shop/address-slice/address-slice.shop.index'
 
 
 const store = configureStore({
@@ -10,7 +11,8 @@ const store = configureStore({
         auth : authReducer,
         adminProducts : AdminProductSlice,
         shopProducts: ShoppingProductSlice,
-        shopCart: ShoppingCartSlice
+        shopCart: ShoppingCartSlice,
+        shopAddress: AddressSlice
     },
 });
 

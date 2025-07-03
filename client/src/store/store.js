1befdaae1a15ea35286e17a9e7238574dfa8c94f
Arjuna Anuhass <anuhass2017@gmail.com>
2025-07-03 17:02:03 +0530

    created the order slice and the redux part of the frontend

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -4,6 +4,7 @@ import AdminProductSlice from './admin/product-slice/product-slice.admin.store.i
 import ShoppingProductSlice from './shop/product-slice/product-slice.shop.index'
 import ShoppingCartSlice from './shop/cart-slice/cart-slice.shop.index'
 import AddressSlice from './shop/address-slice/address-slice.shop.index'
+import ShopOrderSlice from './shop/order-slice/order-slice.shop.index'
 
 
 const store = configureStore({
@@ -12,7 +13,8 @@ const store = configureStore({
         adminProducts : AdminProductSlice,
         shopProducts: ShoppingProductSlice,
         shopCart: ShoppingCartSlice,
-        shopAddress: AddressSlice
+        shopAddress: AddressSlice,
+        shopOrder: ShopOrderSlice
     },
 });
 
